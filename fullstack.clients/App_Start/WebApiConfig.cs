0c895b14f2b10f9693b1a75d43cdd581b3ff223b
Victor Vilela <vilela_victor@hotmail.com>
2019-08-05 22:43:49 -0500

    habilita el cross origin requests CORS en las apis

--- fullstack.clients/App_Start/WebApiConfig.cs
+++ fullstack.clients/App_Start/WebApiConfig.cs
@@ -19,6 +19,8 @@ namespace fullstack.clients
                 routeTemplate: "api/{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
+
+            config.EnableCors();
         }
     }
 }
--- fullstack.kpis/App_Start/WebApiConfig.cs
+++ fullstack.kpis/App_Start/WebApiConfig.cs
@@ -19,6 +19,8 @@ namespace fullstack.kpis
                 routeTemplate: "api/{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
+
+            config.EnableCors();
         }
     }
 }
